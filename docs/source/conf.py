ad2aef06699b9dc691b2a84a5ae00c220f412d58
jcov28 <94916060+jcov28@users.noreply.github.com>
2023-01-05 12:41:35 -0600

    Update conf.py
    Version and author name changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'unid'
+copyright = '2023, jcov28'
+author = 'jcov28'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0.0'
+version = '0.0.0'
 
 # -- General configuration
 
